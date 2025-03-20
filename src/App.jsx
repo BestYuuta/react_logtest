65613f686cffe852d96865573e980b62e2c2f771
ThaiBao <baoanime2005@gmail.com>
2025-03-20 08:01:03 +0700

    feat: fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
 // App.jsx
 import React from "react";
-import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
-import SocialMediaProfile from "./SocialMediaProfile";
-
-const lightTheme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#1976d2",
-    },
-    background: {
-      default: "#fff" ,
-      paper: "#fff",
-    },
-  },
-  typography: {
-    fontFamily: "Roboto, sans-serif",
-  },
-});
+import SignUpPage from "../src/View/SignUpPage";
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-      {/* Reset CSS cho đồng nhất */}
-      <CssBaseline />
-
-      {/* Hiển thị trang SocialMediaProfile */}
-      <SocialMediaProfile />
-    </ThemeProvider>
+    <SignUpPage />
   );
 }
 
